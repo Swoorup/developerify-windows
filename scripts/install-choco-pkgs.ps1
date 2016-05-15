384404a5e63ee2e8610d3c8594d94265e159f1e2
Swoorup Joshi <swoorupj@gmail.com>
2016-05-15 20:04:00 +0800

    Fixed Issues with merge

--- scripts/install-choco-pkgs.ps1
+++ scripts/install-choco-pkgs.ps1
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # install applications
 cinst babun
 cinst f.lux
@@ -10,10 +9,3 @@ cinst regexpixie
 cinst autohotkey
 cinst linqpad
 cinst beyondcompare
-=======
-# install applications
-cinst babun
-cinst f.lux
-cinst atom
-cinst sysinternals
->>>>>>> acc9c5c389fd11e4c0bc7507aa9dbcfe2460466a
